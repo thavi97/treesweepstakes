10e65ca64332163fe364f03c20955ed428bbfb79
Thavi <thavit97@gmail.com>
2021-06-13 20:33:50 +0100

    phase 1 complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,41 +13,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-      $countries = "";
-
-      return view('home', ['countries' => $countries]);
+      return view('home');
     }
 
-    function gJson(){
-
-      $curl = curl_init();
-
-      curl_setopt_array($curl, [
-        CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v3/countries",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_HTTPHEADER => [
-          "x-rapidapi-host: api-football-v1.p.rapidapi.com",
-          "x-rapidapi-key: c16f3bf5abmsh4afe1cc79ab9f4bp1d3446jsnfb142dbfbf19"
-        ],
-      ]);
-
-      $response = curl_exec($curl);
-      $err = curl_error($curl);
-
-      curl_close($curl);
-
-      if ($err) {
-        echo "cURL Error #:" . $err;
-      } else {
-        echo $response;
-      }
-    }
 
     /**
      * Show the form for creating a new resource.
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,28 +6,32 @@
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <link href="css/flexbox-madness.css" rel="stylesheet">
 <link href="css/styles.css" rel="stylesheet">
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <link href='https://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Kaushan+Script|Herr+Von+Muellerhoff' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Istok+Web|Roboto+Condensed:700' rel='stylesheet' type='text/css'>
-
-<title>March Madness Stock Matchup</title>
+<title>TheTree Sweepstakes 2021</title>
 </head>
 <body>
 <header class="hero">
+  <img class="animate__animated animate__fadeInTopLeft img-fluid" src="img/star.png" style="position: absolute;"/>
   <div class="hero-wrap">
-   <p class="intro" id="intro">The Tree Sweepstakes</p>
-   <h1 id="headline">Euro 2020</h1>
-   <p class="year"><i class="fa fa-star"></i> 2021 Edition <i class="fa fa-star"></i></p>
-   <p>Ballin' Outta Control</p>
+   <p class="intro animate__animated animate__backInLeft" id="intro">The Tree Sweepstakes</p>
+   <br>
+   <h1 class="animate__animated animate__backInRight" id="headline">Euro 2020</h1>
+   <p class="year animate__animated animate__backInUp"><i class="fa fa-star"></i> 2021 Edition <i class="fa fa-star"></i></p>
  </div>
 </header>
 
 <section id="groups">
   <div class="container">
-    <div class="col-lg-2">
+    <div class="col-lg-2 animate__animated animate__fadeIn">
       <table class="table">
         <thead>
           <tr>
@@ -36,21 +40,21 @@
         </thead>
         <tbody>
           <tr>
-            <td>Italy <img src="img/4x3/it.svg"/ style="width:20px; height: auto; float:right; margin-top: 5px;"></td>
+            <td><img src="img/4x3/it.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Ramiz<span style="float: right;">3</span></td>
           </tr>
-          <tr class="success">
-            <td>Switzerland</td>
+          <tr>
+            <td><img src="img/4x3/ch.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Izzy<span style="float: right;">1</span></td>
           </tr>
-          <tr class="danger">
-            <td>Wales</td>
+          <tr>
+            <td><img src="img/4x3/gb-wls.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Ed<span style="float: right;">1</span></td>
           </tr>
-          <tr class="info">
-            <td>Turkey</td>
+          <tr>
+            <td><img src="img/4x3/tr.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Ella<span style="float: right;">0</span></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="col-lg-2">
+    <div class="col-lg-2 animate__animated animate__fadeIn">
       <table class="table">
         <thead>
           <tr>
@@ -59,21 +63,21 @@
         </thead>
         <tbody>
           <tr>
-            <td>Belgium</td>
+            <td><img src="img/4x3/be.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Hales<span style="float: right;">3</span></td>
           </tr>
-          <tr class="success">
-            <td>Finland</td>
+          <tr>
+            <td><img src="img/4x3/fi.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Ashe<span style="float: right;">3</span></td>
           </tr>
-          <tr class="danger">
-            <td>Denmark</td>
+          <tr>
+            <td><img src="img/4x3/dk.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Sophie<span style="float: right;">0</span></td>
           </tr>
-          <tr class="info">
-            <td>Russia</td>
+          <tr>
+            <td><img src="img/4x3/ru.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Lauren<span style="float: right;">0</span></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="col-lg-2">
+    <div class="col-lg-2 animate__animated animate__fadeIn">
       <table class="table">
         <thead>
           <tr>
@@ -82,21 +86,21 @@
         </thead>
         <tbody>
           <tr>
-            <td>Austria</td>
+            <td><img src="img/4x3/at.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Lexi<span style="float: right;">3</span></td>
           </tr>
-          <tr class="success">
-            <td>Netherlands</td>
+          <tr>
+            <td><img src="img/4x3/ua.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Elise<span style="float: right;">0</span></td>
           </tr>
-          <tr class="danger">
-            <td>North Macedonia</td>
+          <tr>
+            <td><img src="img/4x3/mk.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Chelsea<span style="float: right;">0</span></td>
           </tr>
-          <tr class="info">
-            <td>Ukraine</td>
+          <tr>
+            <td><img src="img/4x3/nl.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Sam P<span style="float: right;">0</span></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="col-lg-2">
+    <div class="col-lg-2 animate__animated animate__fadeIn">
       <table class="table">
         <thead>
           <tr>
@@ -105,21 +109,21 @@
         </thead>
         <tbody>
           <tr>
-            <td>Croatia</td>
+            <td><img src="img/4x3/gb-eng.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Rob<span style="float: right;">3</span></td>
           </tr>
-          <tr class="success">
-            <td>Success</td>
+          <tr>
+            <td><img src="img/4x3/gb-sct.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Nicole<span style="float: right;">0</span></td>
           </tr>
-          <tr class="danger">
-            <td>Danger</td>
+          <tr>
+            <td><img src="img/4x3/cz.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Felix<span style="float: right;">0</span></td>
           </tr>
-          <tr class="info">
-            <td>Info</td>
+          <tr>
+            <td><img src="img/4x3/hr.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Scobes<span style="float: right;">0</span></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="col-lg-2">
+    <div class="col-lg-2 animate__animated animate__fadeIn">
       <table class="table">
         <thead>
           <tr>
@@ -128,21 +132,21 @@
         </thead>
         <tbody>
           <tr>
-            <td>Default</td>
+            <td><img src="img/4x3/pl.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Tash<span style="float: right;">0</span></td>
           </tr>
-          <tr class="success">
-            <td>Success</td>
+          <tr>
+            <td><img src="img/4x3/sk.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Menuka<span style="float: right;">0</span></td>
           </tr>
-          <tr class="danger">
-            <td>Danger</td>
+          <tr>
+            <td><img src="img/4x3/es.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Ryan<span style="float: right;">0</span></td>
           </tr>
-          <tr class="info">
-            <td>Info</td>
+          <tr>
+            <td><img src="img/4x3/se.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Thavi<span style="float: right;">0</span></td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="col-lg-2">
+    <div class="col-lg-2 animate__animated animate__fadeIn">
       <table class="table">
         <thead>
           <tr>
@@ -151,16 +155,16 @@
         </thead>
         <tbody>
           <tr>
-            <td>Default</td>
+            <td><img src="img/4x3/fr.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Alan<span style="float: right;">0</span></td>
           </tr>
-          <tr class="success">
-            <td>Success</td>
+          <tr>
+            <td><img src="img/4x3/de.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Sam Sophie<span style="float: right;">0</span></td>
           </tr>
-          <tr class="danger">
-            <td>Danger</td>
+          <tr>
+            <td><img src="img/4x3/hu.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Laura<span style="float: right;">0</span></td>
           </tr>
-          <tr class="info">
-            <td>Info</td>
+          <tr>
+            <td><img src="img/4x3/pt.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Emilie<span style="float: right;">0</span></td>
           </tr>
         </tbody>
       </table>
@@ -171,44 +175,9 @@
 <section id="bracket">
 <div class="container">
 <div class="split split-one">
-  <div class="round round-one current">
-    <div class="round-details">Round 1<br/><span class="date">March 16</span></div>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">76</span></li>
-      <li class="team team-bottom"><span class="score">82</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">64</span></li>
-      <li class="team team-bottom"><span class="score">56</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">68</span></li>
-      <li class="team team-bottom"><span class="score">54</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">74</span></li>
-      <li class="team team-bottom"><span class="score">92</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">78</span></li>
-      <li class="team team-bottom"><span class="score">80</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">64</span></li>
-      <li class="team team-bottom"><span class="score">63</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">70</span></li>
-      <li class="team team-bottom"><span class="score">59</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">64</span></li>
-      <li class="team team-bottom"><span class="score">68</span></li>
-    </ul>
-  </div>	<!-- END ROUND ONE -->
 
   <div class="round round-two">
-    <div class="round-details">Round 2<br/><span class="date">March 18</span></div>
+    <div class="round-details">Round of 16<br/><span class="date">June 26-29</span></div>
     <ul class="matchup">
       <li class="team team-top">&nbsp;<span class="score">&nbsp;</span></li>
       <li class="team team-bottom">&nbsp;<span class="score">&nbsp;</span></li>
@@ -228,7 +197,7 @@
   </div>	<!-- END ROUND TWO -->
 
   <div class="round round-three">
-    <div class="round-details">Round 3<br/><span class="date">March 22</span></div>
+    <div class="round-details">Quarter Finals<br/><span class="date">July 2-3</span></div>
     <ul class="matchup">
       <li class="team team-top">&nbsp;<span class="score">&nbsp;</span></li>
       <li class="team team-bottom">&nbsp;<span class="score">&nbsp;</span></li>
@@ -242,7 +211,7 @@
 
 <div class="champion">
   <div class="semis-l">
-    <div class="round-details">west semifinals <br/><span class="date">March 26-28</span></div>
+    <div class="round-details">Semi Final 1<br/><span class="date">July 6</span></div>
     <ul class ="matchup championship">
       <li class="team team-top">&nbsp;<span class="vote-count">&nbsp;</span></li>
       <li class="team team-bottom">&nbsp;<span class="vote-count">&nbsp;</span></li>
@@ -250,14 +219,14 @@
   </div>
   <div class="final">
     <i class="fa fa-trophy"></i>
-    <div class="round-details">championship <br/><span class="date">March 30 - Apr. 1</span></div>
+    <div class="round-details">Final<br/><span class="date">July 11</span></div>
     <ul class ="matchup championship">
       <li class="team team-top">&nbsp;<span class="vote-count">&nbsp;</span></li>
       <li class="team team-bottom">&nbsp;<span class="vote-count">&nbsp;</span></li>
     </ul>
   </div>
   <div class="semis-r">
-    <div class="round-details">east semifinals <br/><span class="date">March 26-28</span></div>
+    <div class="round-details">Semi Final 2<br/><span class="date">July 7</span></div>
     <ul class ="matchup championship">
       <li class="team team-top">&nbsp;<span class="vote-count">&nbsp;</span></li>
       <li class="team team-bottom">&nbsp;<span class="vote-count">&nbsp;</span></li>
@@ -267,10 +236,8 @@
 
 
 <div class="split split-two">
-
-
   <div class="round round-three">
-    <div class="round-details">Round 3<br/><span class="date">March 22</span></div>
+    <div class="round-details">Quarter Finals<br/><span class="date">July 2-3</span></div>
     <ul class="matchup">
       <li class="team team-top">&nbsp;<span class="score">&nbsp;</span></li>
       <li class="team team-bottom">&nbsp;<span class="score">&nbsp;</span></li>
@@ -282,7 +249,7 @@
   </div>	<!-- END ROUND THREE -->
 
   <div class="round round-two">
-    <div class="round-details">Round 2<br/><span class="date">March 18</span></div>
+    <div class="round-details">Round of 16<br/><span class="date">June 26-29</span></div>
     <ul class="matchup">
       <li class="team team-top">&nbsp;<span class="score">&nbsp;</span></li>
       <li class="team team-bottom">&nbsp;<span class="score">&nbsp;</span></li>
@@ -300,41 +267,6 @@
       <li class="team team-bottom">&nbsp;<span class="score">&nbsp;</span></li>
     </ul>
   </div>	<!-- END ROUND TWO -->
-  <div class="round round-one current">
-    <div class="round-details">Round 1<br/><span class="date">March 16</span></div>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">62</span></li>
-      <li class="team team-bottom"><span class="score">54</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">68</span></li>
-      <li class="team team-bottom"><span class="score">66</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">64</span></li>
-      <li class="team team-bottom"><span class="score">56</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">36</span></li>
-      <li class="team team-bottom"><span class="score">40</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">38</span></li>
-      <li class="team team-bottom"><span class="score">44</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">52</span></li>
-      <li class="team team-bottom"><span class="score">80</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">58</span></li>
-      <li class="team team-bottom"><span class="score">59</span></li>
-    </ul>
-    <ul class="matchup">
-      <li class="team team-top"><span class="score">74</span></li>
-      <li class="team team-bottom"><span class="score">111</span></li>
-    </ul>
-  </div>	<!-- END ROUND ONE -->
 </div>
 </div>
 </section>
