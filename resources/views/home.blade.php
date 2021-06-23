ee9ea2d383c69673326cf75968a5d4fb04b02ec1
Thavi <thavit97@gmail.com>
2021-06-23 09:03:06 +0100

    england and croatia win

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -112,13 +112,13 @@
         </thead>
         <tbody>
           <tr>
+            <td><img src="img/4x3/gb-eng.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Rob<span style="float: right;">7</span></td>
+          </tr>
+          <tr>
+            <td><img src="img/4x3/hr.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Scobes<span style="float: right;">4</span></td>
+          </tr>
+          <tr>
             <td><img src="img/4x3/cz.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Felix<span style="float: right;">4</span></td>
-          </tr>
-          <tr>
-            <td><img src="img/4x3/gb-eng.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Rob<span style="float: right;">4</span></td>
-          </tr>
-          <tr>
-            <td><img src="img/4x3/hr.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Scobes<span style="float: right;">1</span></td>
           </tr>
           <tr>
             <td><img src="img/4x3/gb-sct.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Nicole<span style="float: right;">1</span></td>
