d79bf030109e491cb7c857b6acd5b0d392b19a80
Thavi <thavit97@gmail.com>
2021-07-02 20:05:40 +0100

    spain win

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -202,8 +202,8 @@
   <div class="round round-three">
     <div class="round-details">Quarter Finals<br/><span class="date">July 2-3</span></div>
     <ul class="matchup">
-      <li class="team team-top"><img src="img/4x3/ch.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Izzy<span class="score">&nbsp;</span></li>
-      <li class="team team-bottom"><img src="img/4x3/es.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Ryan<span class="score">&nbsp;</span></li>
+      <li class="team team-top" style="background-color: rgba(223, 34, 34, 0.4);"><img src="img/4x3/ch.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Izzy<span class="score">1 (1)</span></li>
+      <li class="team team-bottom" style="background-color: rgba(45, 189, 57, 0.4);"><img src="img/4x3/es.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Ryan<span class="score">1 (3)</span></li>
     </ul>
     <ul class="matchup">
       <li class="team team-top"><img src="img/4x3/cz.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Felix<span class="score">&nbsp;</span></li>
@@ -216,7 +216,7 @@
   <div class="semis-l">
     <div class="round-details">Semi Final 1<br/><span class="date">July 6</span></div>
     <ul class ="matchup championship">
-      <li class="team team-top">&nbsp;<span class="vote-count">&nbsp;</span></li>
+      <li class="team team-top"><img src="img/4x3/es.svg"/ style="width:20px; height: auto; float:left; margin: 5px 5px 0 0;">Ryan<span class="score">&nbsp;</span></li>
       <li class="team team-bottom">&nbsp;<span class="vote-count">&nbsp;</span></li>
     </ul>
   </div>
